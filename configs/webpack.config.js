636c32008f98ac97f48021ecff66363a66da2458
btinoco <btinoco.dev@gmail.com>
2017-07-21 02:53:48 -0500

    add app component

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -1,16 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-console.log(path.resolve(__dirname, './../node_modules/'));
-console.log(path.resolve(__dirname, './..'));
-
-
 const entryPath = path.resolve(process.cwd(), './src/index.js');
 const outputPath = path.resolve(process.cwd(), './dist');
 const indexPath = path.resolve(process.cwd(), './index.html');
 
 module.exports = {
-    context: path.join(__dirname, './..'),
     entry: entryPath,
     output: {
         path: outputPath,
@@ -60,9 +55,6 @@ module.exports = {
         })
     ],
     resolve: {
-        modules: [
-            path.resolve(__dirname, './../node_modules')
-        ],
         extensions: ['.js', '.jsx', '.css']
     }
 };
