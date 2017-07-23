77d43c01225eacec1b309314072e1914cbc83331
btinoco <btinoco.dev@gmail.com>
2017-07-23 03:18:42 -0500

    added new config file for project

---
+++ configs/react-xtruct.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    project: {
+        name: 'unknown',
+        root: './src',
+        index: './index.html',
+        environments: {
+            dev: '',
+            stage: '',
+            prod: ''
+        },
+        styles: 'css'
+    },
+    scripts: [],
+    options: {}
+};
