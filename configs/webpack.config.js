56aff6dc97513cd593338b43132d5c001d9671a0
btinoco <btinoco.dev@gmail.com>
2017-07-24 19:45:11 -0500

    refactored all commands and main react-xtruct

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -137,9 +137,3 @@ module.exports = {
         extensions: ['.js', '.jsx', '.css', '.sass', '.less', '.scss']
     }
 };
-
-
-// {
-//     test: /\.css$/,
-//         loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader')
-// },
