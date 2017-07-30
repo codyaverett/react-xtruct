965fb187084961d8a51322239fcd1bef64491ac5
btinoco <btinoco.dev@gmail.com>
2017-07-30 00:30:12 -0500

    version 0.0.20

--- react-xtruct.js
+++ react-xtruct.js
@@ -6,7 +6,7 @@ const program = require('commander');
 const commands = require('./commands');
 const common = require('./commands/common');
 
-const version = '0.0.19';
+const version = '0.0.20';
 
 program
     .version(chalk.green(version));
