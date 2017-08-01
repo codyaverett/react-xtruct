f7ac8985c3a3b35274361f03d444777e8ba95ae9
btinoco <btinoco.dev@gmail.com>
2017-07-31 23:40:24 -0500

    init test command implementation for react-xtruct

---
+++ configs/karma.conf.js
@@ -0,0 +1,55 @@
+const path = require('path');
+
+module.exports = function (config) {
+    config.set({
+        basePath: '',
+        browsers: ['PhantomJS'],
+        frameworks: ['mocha'],
+        files: [
+            'node_modules/babel-polyfill/dist/polyfill.js',
+            'src/**/*.js'
+        ],
+        preprocessors: {
+            'src/public/AppEntry.js': ['webpack', 'sourcemap'],
+            'test/**/*.js': ['webpack', 'sourcemap']
+        },
+        webpack: {
+            devtool: 'inline-source-map',
+            module: {
+                loaders: [
+                    {
+                        test: /\.js$/,
+                        loader: 'babel-loader',
+                        exclude: path.resolve(__dirname, 'node_modules'),
+                        query: {
+                            plugins: ['transform-decorators-legacy', 'transform-regenerator'],
+                            presets: ['react', 'es2015', 'stage-1']
+                        }
+                    },
+                    {
+                        test: /\.json$/,
+                        loader: 'json-loader',
+                    },
+                ]
+            },
+            externals: {
+                'react/addons': true,
+                'react/lib/ExecutionEnvironment': true,
+                'react/lib/ReactContext': true
+            }
+        },
+        webpackServer: {
+            noInfo: true
+        },
+        reporters: ['nyan'],
+        nyanReporter: {
+            suppressErrorHighlighting: true,
+        },
+
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        singleRun: false,
+    });
+};
