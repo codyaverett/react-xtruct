750ff7bece21d49046d4d66256b6f414830ea436
btinoco <btinoco.dev@gmail.com>
2017-09-17 15:30:06 -0500

    refactored code for generate

--- commands/generate.js
+++ commands/generate.js
@@ -21,7 +21,6 @@ class Generate {
             const redux = options.cmd.redux || common.readLocalConfig().project.redux;
             const router = options.cmd.router || common.readLocalConfig().project.router;
             let successfulMessage = 'Component generated successful!';
-            let startingPath = `./src/${options.name.replace(/\.\//, '')}`;
 
             mkdirp.sync(componentPath);
 
@@ -39,8 +38,6 @@ class Generate {
                 outputPath: componentPath
             });
 
-            console.log(`- ${chalk.yellow(`${startingPath}/${componentName}.component.jsx`)} ${chalk.green('created successfully')}.`);
-
             template.compile({
                 templateDirectory: templatePath,
                 templateFilename: 'spec_',
@@ -52,8 +49,6 @@ class Generate {
                 outputPath: componentPath
             });
 
-            console.log(`- ${chalk.yellow(`${startingPath}/${componentName}.component.spec.jsx`)} ${chalk.green('created successfully')}.`);
-
             template.compileCSS({
                 style: options.cmd.style,
                 templateDirectory: templatePath,
@@ -61,8 +56,6 @@ class Generate {
                 outputFilename: `${componentName}.styles`,
                 outputPath: componentPath
             });
-
-            console.log(`- ${chalk.yellow(`${startingPath}/${componentName}.styles.${style}`)} ${chalk.green('created successfully')}.`);
 
             if (redux) {
                 template.compile({
@@ -76,8 +69,6 @@ class Generate {
                     outputPath: componentPath
                 });
 
-                console.log(`- ${chalk.yellow(`${startingPath}/${componentName}.actions.js`)} ${chalk.green('created successfully')}.`);
-
                 template.compile({
                     templateDirectory: templatePath,
                     templateFilename: 'reducers_',
@@ -87,8 +78,6 @@ class Generate {
                     outputFilename: `${componentName}.reducers.js`,
                     outputPath: componentPath
                 });
-
-                console.log(`- ${chalk.yellow(`${startingPath}/${componentName}.reducers.js`)} ${chalk.green('created successfully')}.`);
             }
 
             if (redux && !router) {
